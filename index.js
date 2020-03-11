10acee82227c0da2f6805b682d4fe1617bcd09c0
Vladislav Ivanov <himametsu@gmail.com>
2020-03-12 00:20:03 +0300

    feat: create files

---
+++ index.js
@@ -0,0 +1,521 @@
+const state1 = {
+  row1: [
+    {
+      name: 'Backquote',
+      code: 'Backquote',
+      content: [
+        {
+          lang: 'rus',
+          down: 'ё',
+          up: 'Ё',
+        },
+        {
+          lang: 'eng',
+          down: '`',
+          up: '~',
+        }
+      ],
+    },
+    {
+      name: 'Digit1',
+      code: 'Digit1',
+      content: [
+        {
+          lang: 'rus',
+          down: '1',
+          up: '!',
+        },
+        {
+          lang: 'eng',
+          down: '1',
+          up: '!',
+        }
+      ],
+    },
+    {
+      name: 'Digit2',
+      code: 'Digit2',
+      content: [
+        {
+          lang: 'rus',
+          down: '2',
+          up: '"',
+        },
+        {
+          lang: 'eng',
+          down: '2',
+          up: '@',
+        }
+      ],
+    },
+    {
+      name: 'Digit3',
+      code: 'Digit3',
+      content: [
+        {
+          lang: 'rus',
+          down: '3',
+          up: '№',
+        },
+        {
+          lang: 'eng',
+          down: '3',
+          up: '#',
+        }
+      ],
+    },
+    {
+      name: 'Digit4',
+      code: 'Digit4',
+      content: [
+        {
+          lang: 'rus',
+          down: '4',
+          up: ';',
+        },
+        {
+          lang: 'eng',
+          down: '4',
+          up: '$',
+        }
+      ],
+    },
+    {
+      name: 'Digit5',
+      code: 'Digit5',
+      content: [
+        {
+          lang: 'rus',
+          down: '5',
+          up: '%',
+        },
+        {
+          lang: 'eng',
+          down: '5',
+          up: '%',
+        }
+      ],
+    },
+    {
+      name: 'Digit6',
+      code: 'Digit6',
+      content: [
+        {
+          lang: 'rus',
+          down: '6',
+          up: ':',
+        },
+        {
+          lang: 'eng',
+          down: '6',
+          up: '^',
+        }
+      ],
+    },
+    {
+      name: 'Digit7',
+      code: 'Digit7',
+      content: [
+        {
+          lang: 'rus',
+          down: '7',
+          up: '?',
+        },
+        {
+          lang: 'eng',
+          down: '7',
+          up: '&',
+        }
+      ],
+    },
+    {
+      name: 'Digit8',
+      code: 'Digit8',
+      content: [
+        {
+          lang: 'rus',
+          down: '8',
+          up: '*',
+        },
+        {
+          lang: 'eng',
+          down: '8',
+          up: '*',
+        }
+      ],
+    },
+    {
+      name: 'Digit9',
+      code: 'Digit9',
+      content: [
+        {
+          lang: 'rus',
+          down: '9',
+          up: '(',
+        },
+        {
+          lang: 'eng',
+          down: '9',
+          up: '(',
+        }
+      ],
+    },
+    {
+      name: 'Digit0',
+      code: 'Digit0',
+      content: [
+        {
+          lang: 'rus',
+          down: '0',
+          up: ')',
+        },
+        {
+          lang: 'eng',
+          down: '0',
+          up: ')',
+        }
+      ],
+    },
+    {
+      name: 'Minus',
+      code: 'Minus',
+      content: [
+        {
+          lang: 'rus',
+          down: '-',
+          up: '_',
+        },
+        {
+          lang: 'eng',
+          down: '-',
+          up: '_',
+        }
+      ],
+    },
+    {
+      name: 'Equal',
+      code: 'Equal',
+      content: [
+        {
+          lang: 'rus',
+          down: '=',
+          up: '+',
+        },
+        {
+          lang: 'eng',
+          down: '=',
+          up: '+',
+        }
+      ],
+    },
+    {
+      name: 'Backspace',
+      code: 'Backspace',
+      content: [
+        {
+          lang: 'rus',
+          down: 'Backspace',
+          up: 'Backspace',
+        },
+        {
+          lang: 'eng',
+          down: 'Backspace',
+          up: 'Backspace',
+        }
+      ],
+    },
+  ],
+  row2: [
+    {
+      name: 'Tab',
+      code: 'Tab',
+      content: [
+        {
+          lang: 'rus',
+          down: 'tab',
+          up: 'Tab',
+        },
+        {
+          lang: 'eng',
+          down: 'tab',
+          up: 'Tab',
+        }
+      ],
+    },
+    {
+      name: 'Q',
+      code: 'KeyQ',
+      content: [
+        {
+          lang: 'rus',
+          down: 'й',
+          up: 'Й',
+        },
+        {
+          lang: 'eng',
+          down: 'q',
+          up: 'Q',
+        }
+      ],
+    },
+    {
+      name: 'W',
+      code: 'KeyW',
+      content: [
+        {
+          lang: 'rus',
+          down: 'ц',
+          up: 'Ц',
+        },
+        {
+          lang: 'eng',
+          down: 'w',
+          up: 'W',
+        }
+      ],
+    },
+    {
+      name: 'E',
+      code: 'KeyE',
+      content: [
+        {
+          lang: 'rus',
+          down: 'у',
+          up: 'У',
+        },
+        {
+          lang: 'eng',
+          down: 'e',
+          up: 'E',
+        }
+      ],
+    },
+    {
+      name: 'R',
+      code: 'KeyR',
+      content: [
+        {
+          lang: 'rus',
+          down: 'к',
+          up: 'К',
+        },
+        {
+          lang: 'eng',
+          down: 'r',
+          up: 'R',
+        }
+      ],
+    },
+    {
+      name: 'T',
+      code: 'KeyT',
+      content: [
+        {
+          lang: 'rus',
+          down: 'е',
+          up: 'Е',
+        },
+        {
+          lang: 'eng',
+          down: 't',
+          up: 'T',
+        }
+      ],
+    },
+    {
+      name: 'Y',
+      code: 'KeyY',
+      content: [
+        {
+          lang: 'rus',
+          down: 'н',
+          up: 'Н',
+        },
+        {
+          lang: 'eng',
+          down: 'y',
+          up: 'Y',
+        }
+      ],
+    },
+    {
+      name: 'U',
+      code: 'KeyU',
+      content: [
+        {
+          lang: 'rus',
+          down: 'г',
+          up: 'Г',
+        },
+        {
+          lang: 'eng',
+          down: 'u',
+          up: 'u',
+        }
+      ],
+    },
+    {
+      name: 'I',
+      code: 'i',
+      content: [
+        {
+          lang: 'rus',
+          down: 'ш',
+          up: 'Ш',
+        },
+        {
+          lang: 'eng',
+          down: 'i',
+          up: 'I',
+        }
+      ],
+    },
+    {
+      name: 'O',
+      code: 'KeyO',
+      content: [
+        {
+          lang: 'rus',
+          down: 'щ',
+          up: 'Щ',
+        },
+        {
+          lang: 'eng',
+          down: 'o',
+          up: 'O',
+        }
+      ],
+    },
+    {
+      name: 'P',
+      code: 'KeyP',
+      content: [
+        {
+          lang: 'rus',
+          down: 'з',
+          up: 'З',
+        },
+        {
+          lang: 'eng',
+          down: 'p',
+          up: 'P',
+        }
+      ],
+    },
+    {
+      name: 'BracketLeft',
+      code: 'BracketLeft',
+      content: [
+        {
+          lang: 'rus',
+          down: 'х',
+          up: 'Х',
+        },
+        {
+          lang: 'eng',
+          down: '[',
+          up: '{',
+        }
+      ],
+    },
+    {
+      name: 'BracketRight',
+      code: 'BracketRight',
+      content: [
+        {
+          lang: 'rus',
+          down: 'ъ',
+          up: 'Ъ',
+        },
+        {
+          lang: 'eng',
+          down: ']',
+          up: '}',
+        }
+      ],
+    },
+    {
+      name: 'Backslash',
+      code: 'Backslash',
+      content: [
+        {
+          lang: 'rus',
+          down: '\\',
+          up: '//',
+        },
+        {
+          lang: 'eng',
+          down: '\\',
+          up: '||',
+        }
+      ],
+    },
+  ]
+}
+
+window.onload = function () {
+  console.log('Дом дерево загрузилось!')
+  renderContainer()
+}
+
+renderContainer = () => {
+  const container = document.createElement('div')
+  container.classList.add('container')
+  const textContainer = document.createTextNode('Container')
+  container.appendChild(textContainer)
+  document.body.appendChild(container)
+
+  const textContent = document.createElement('div')
+  textContent.classList.add('textContent')
+
+  const text = document.createElement('textarea')
+  text.classList.add('textarea')
+  textContent.appendChild(text)
+
+  container.appendChild(textContent)
+
+  const keyboard = document.createElement('div')
+  keyboard.classList.add('keyboard')
+  container.appendChild(keyboard)
+
+  for (let key in state1) {
+    const row = document.createElement('div')
+    row.classList.add(`${key}`)
+    keyboard.appendChild(row)
+
+    state1[key].map((item) => {
+      const key = document.createElement('div')
+      key.classList.add('key')
+      key.classList.add(item.name)
+      row.appendChild(key)
+
+      const rus = document.createElement('span')
+      rus.classList.add('rus')
+      key.appendChild(rus)
+
+      const eng = document.createElement('span')
+      eng.classList.add('eng')
+      key.appendChild(eng)
+
+      for (let i = 0; i < item.content.length; i++) {
+        const down = document.createElement('span')
+        down.classList.add('down')
+        const downText = document.createTextNode(item.content[i].down)
+        down.appendChild(downText)
+
+        const up = document.createElement('span')
+        up.classList.add('up')
+        const upText = document.createTextNode(item.content[i].up)
+        up.appendChild(upText)
+
+        const currentString = [
+          rus,
+          eng
+        ]
+
+        currentString[i].appendChild(down)
+        currentString[i].appendChild(up)
+      }
+    })
+  }
+}
